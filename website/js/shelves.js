3421be6f850280f9886542ebf349c661e5b0d47e
n226 <64910220+n226@users.noreply.github.com>
2021-04-23 13:32:56 +0200

    Web v2

--- website/js/shelves.js
+++ website/js/shelves.js
@@ -1,37 +1,19 @@
 
-var totalShelves = 96;
+var totalShelves = 72;
 var shelvesPerRow = 6;
 var shelvesPerCorridor = 4;
 
-function getShelveDatabaseData(){
-    for (var i = 0; i < totalShelves; i++) {
-        //TODO: obtener las 3 variables de la DB
+const e1 = "#42f56f";
+const e2 = "#ebcc42";
+const e3 = "#eb9742";
+const e4 = "#eb4842";
+const e5 = "#962a2a";
 
-        var state = 0;//Math.floor(Math.random() * 4);
-        var incomplete_orders = Math.floor(Math.random() * 100);
-        var stock = Math.floor(Math.random() * 100);
-
-        if (incomplete_orders > stock){
-            state = 4;
-        }
-        else if(stock === 0){
-            state = 3;
-        }
-        else if (incomplete_orders < stock && incomplete_orders >= (stock*0.5)){
-            state = 2;
-        }
-        else if (incomplete_orders > 0){
-            state = 1;
-        }
-        else{
-            state = 0;
-        }
-
-        shelve_states.push(state);
-        shelve_orders.push(incomplete_orders);
-        shelve_stocks.push(stock);
-    }
-}
+var shelve_buttons = [];
+var shelve_states = [];
+var shelve_orders = [];
+var shelve_stocks = [];
+var shelve_orders_data = [];
 
 function getRealShelveState(id){
     return shelve_states[id];
@@ -44,17 +26,6 @@ function getRealInCompleteOrders(id){
 function getRealStock(id){
     return shelve_stocks[id];
 }
-
-const e1 = "#42f56f";
-const e2 = "#ebcc42";
-const e3 = "#eb9742";
-const e4 = "#eb4842";
-const e5 = "#962a2a";
-
-var shelve_buttons = [];
-var shelve_states = [];
-var shelve_orders = [];
-var shelve_stocks = [];
 
 function getColorByState(state){
     switch (state){
@@ -71,13 +42,70 @@ function getColorByState(state){
     }
 }
 
+function calculateState(incomplete_orders, stock){
+    var state = 0;
+
+    if (incomplete_orders > stock){
+        state = 4;
+    }
+    else if(stock === 0){
+        state = 3;
+    }
+    else if (incomplete_orders <= stock && incomplete_orders >= (stock*0.5)){
+        state = 2;
+    }
+    else if (incomplete_orders > 0){
+        state = 1;
+    }
+    else{
+        state = 0;
+    }
+
+    return state;
+}
+
+function getOrderGeneralInfo(id, incomplete_orders, orderState){
+    return "Shelf ID: " + id + " - Orders: " + incomplete_orders + " - State: " + orderState;
+}
+
+function getShelveDatabaseData(){
+    for (var i = 0; i < totalShelves; i++) {
+
+        //TODO: obtener las 3 variables de la DB
+        var incomplete_orders = 0;
+        if (Math.random() > 0.8) {
+            incomplete_orders = Math.floor(Math.random() * 10);
+        }
+        var stock = Math.floor(Math.random() * 15);
+        var orderState = 0;
+
+        // TODO: obtener info pedidos de la DB
+        if(incomplete_orders>0) {
+            new_orders = [i, getOrderGeneralInfo(i, incomplete_orders, orderState)];
+        }else{
+            new_orders = "0";
+        }
+        /*new_orders = [];
+        for(var j=0; j < incomplete_orders; j++){
+            new_orders.push(j);
+        }*/
+
+        shelve_orders_data.push(new_orders)
+
+        var state = calculateState(incomplete_orders, stock);
+        shelve_states.push(state);
+        shelve_orders.push(incomplete_orders);
+        shelve_stocks.push(stock);
+    }
+}
+
 function addShelve(id, container, state, incomplete_orders, stock) {
     var button = document.createElement("button");
     button.type = "button";
     button.className = "estanteria";
     button.name = id;
+    button.id = "shelvesMapButton" + id;
     button.style.backgroundColor = getColorByState(state);
-    button.onclick = function() { checkShelve(id); };
 
     button.innerHTML = incomplete_orders + "-" + stock;
 
@@ -90,6 +118,9 @@ getShelveDatabaseData();
 
 var rows = totalShelves/shelvesPerRow/shelvesPerCorridor;
 var id = 0;
+var shelves_per_section = shelvesPerRow * shelvesPerCorridor;
+var shelve_sections = totalShelves / shelves_per_section;
+
 
 for (var i = 0; i < rows; i++) {
 
